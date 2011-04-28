4b84eb05e7ba5b8139e1a2121bf8217d10fb71d7
Philipp Krenn <pk@xeraa.net>
2011-04-29 01:31:53 +0200

    Translations added

---
+++ lang/de_DE.php
@@ -0,0 +1,48 @@
+<?php
+
+global $lang;
+
+$lang['de_DE']['CustomImage']['PLURALNAME'] = array(
+	'Angepasste Bilder',
+	50,
+	'Pural name of the object, used in dropdowns and to generally identify a collection of this object in the interface'
+);
+$lang['de_DE']['CustomImage']['SINGULARNAME'] = array(
+	'Angepasstes Bild',
+	50,
+	'Singular name of the object, used in dropdowns and to generally identify a single object in the interface'
+);
+$lang['de_DE']['GalleryImage']['PLURALNAME'] = array(
+	'Galerie Bilder',
+	50,
+	'Pural name of the object, used in dropdowns and to generally identify a collection of this object in the interface'
+);
+$lang['de_DE']['GalleryImage']['SINGULARNAME'] = array(
+	'Galerie Bild',
+	50,
+	'Singular name of the object, used in dropdowns and to generally identify a single object in the interface'
+);
+$lang['de_DE']['GalleryPage']['PLURALNAME'] = array(
+	'Galerie Seiten',
+	50,
+	'Pural name of the object, used in dropdowns and to generally identify a collection of this object in the interface'
+);
+$lang['de_DE']['GalleryPage']['SINGULARNAME'] = array(
+	'Galerie Seite',
+	50,
+	'Singular name of the object, used in dropdowns and to generally identify a single object in the interface'
+);
+$lang['de_DE']['GalleryPage']['TITLE'] = 'Titel';
+$lang['de_DE']['Intro.ss']['NUMBER'] = 'Intro Bild';
+$lang['de_DE']['IntroPage']['PLURALNAME'] = array(
+	'Intro Seiten',
+	50,
+	'Pural name of the object, used in dropdowns and to generally identify a collection of this object in the interface'
+);
+$lang['de_DE']['IntroPage']['SINGULARNAME'] = array(
+	'Intro Seite',
+	50,
+	'Singular name of the object, used in dropdowns and to generally identify a single object in the interface'
+);
+
+?>
---
+++ lang/en_US.php
@@ -0,0 +1,48 @@
+<?php
+
+global $lang;
+
+$lang['en_US']['CustomImage']['PLURALNAME'] = array(
+	'Custom Images',
+	50,
+	'Pural name of the object, used in dropdowns and to generally identify a collection of this object in the interface'
+);
+$lang['en_US']['CustomImage']['SINGULARNAME'] = array(
+	'Custom Image',
+	50,
+	'Singular name of the object, used in dropdowns and to generally identify a single object in the interface'
+);
+$lang['en_US']['GalleryImage']['PLURALNAME'] = array(
+	'Gallery Images',
+	50,
+	'Pural name of the object, used in dropdowns and to generally identify a collection of this object in the interface'
+);
+$lang['en_US']['GalleryImage']['SINGULARNAME'] = array(
+	'Gallery Image',
+	50,
+	'Singular name of the object, used in dropdowns and to generally identify a single object in the interface'
+);
+$lang['en_US']['GalleryPage']['PLURALNAME'] = array(
+	'Gallery Pages',
+	50,
+	'Pural name of the object, used in dropdowns and to generally identify a collection of this object in the interface'
+);
+$lang['en_US']['GalleryPage']['SINGULARNAME'] = array(
+	'Gallery Page',
+	50,
+	'Singular name of the object, used in dropdowns and to generally identify a single object in the interface'
+);
+$lang['en_US']['GalleryPage']['TITLE'] = 'Title';
+$lang['en_US']['Intro.ss']['NUMBER'] = 'Intro image';
+$lang['en_US']['IntroPage']['PLURALNAME'] = array(
+	'Intro Pages',
+	50,
+	'Pural name of the object, used in dropdowns and to generally identify a collection of this object in the interface'
+);
+$lang['en_US']['IntroPage']['SINGULARNAME'] = array(
+	'Intro Page',
+	50,
+	'Singular name of the object, used in dropdowns and to generally identify a single object in the interface'
+);
+
+?>
